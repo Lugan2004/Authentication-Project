5a8e9a9fca97c917df76cfdb616fce83ad8e7586
Lugan2004 <visagielugan42@gmail.com>
2025-08-01 16:49:54 +0200

    Adds Google sign-in support
    Implements Google authentication provider alongside GitHub to enhance user sign-in options.
    
    Updates the sign-in page to include a Google sign-in button, providing users with a more seamless authentication experience.

--- auth/src/app/lib/auth.ts
+++ auth/src/app/lib/auth.ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import GitHub from "next-auth/providers/github";
 import { prisma } from "./prisma"; // or "./lib/prisma"
 import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
 
 
 const config ={
@@ -12,11 +13,15 @@ const config ={
             clientId: process.env.GITHUB_ID!,
             clientSecret: process.env.GITHUB_SECRET!,
         }),
+        Google({
+            clientId: process.env.GOOGLE_ID!,
+            clientSecret: process.env.GOOGLE_SECRET!,
+        }),
     ],
     callbacks: {
         session: async ({ session, token}) => {
             if (session?.user && token?.sub){
-                session.user.id =token.sub
+                session.user.id = token.sub
             }
             return session
         },
